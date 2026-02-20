12d89af7995dc5857530d252f9abaaf413647fb8
Agam Bhardwaj <189280292+Overburn-06@users.noreply.github.com>
2026-02-21 00:20:12 +0530

    0051-N-QUEENS Stats: Time: 2 ms (80.53%), Space: 46.9 MB (13.41%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,32 +1,32 @@
 class Solution {
-    static boolean isSafe(int col,int row,char [][]board,int n){
-        for(int j=0;j<col;j++){
-            if(board[row][j]=='Q') return false;
-        }
-        for(int i=row,j=col;i>=0&&j>=0;j--,i--){
-            if(board[i][j]=='Q') return false;
-        }
-        for(int i=row,j=col;i<n && j>=0;i++,j--){
-            if(board[i][j]=='Q') return false;
-        }
-        return true;
-    }
-    static void nqueen(int col,int n,char[][]board,List<List<String>>res){
+    static void nqueens(int col,int n,char[][]board,List<List<String>>res){
         if(col==n){
             ArrayList<String>arr=new ArrayList<>();
             for(int i=0;i<n;i++){
                 arr.add(new String(board[i]));
             }
             res.add(arr);
-            return ;
+            return;
         }
         for(int row=0;row<n;row++){
-            if(isSafe(col,row,board,n)){
+            if(isSafe(row,col,n,board)){
                 board[row][col]='Q';
-                nqueen(col+1,n,board,res);
+                nqueens(col+1,n,board,res);
                 board[row][col]='.';
             }
         }
+    }
+    static boolean isSafe(int row,int col,int n,char [][]board){
+        for(int i=0;i<col;i++){
+            if(board[row][i]=='Q') return false;
+        }
+        for(int i=row,j=col;i>=0&&j>=0;i--,j--){
+            if(board[i][j]=='Q') return false;
+        }
+        for(int i=row,j=col;i<n&&j>=0;i++,j--){
+            if(board[i][j]=='Q') return false;
+        }
+        return true;
     }
     public List<List<String>> solveNQueens(int n) {
         char [][]board =new char[n][n];
@@ -34,7 +34,7 @@ class Solution {
         for(int i=0;i<n;i++){
             Arrays.fill(board[i], '.');
         }  
-        nqueen(0,n,board,res);
+        nqueens(0,n,board,res);
         return res;
     }
 }
