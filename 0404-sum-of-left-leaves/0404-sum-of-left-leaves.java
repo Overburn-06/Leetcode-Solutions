c79b7b2bcd01bf274234b444d06fad7a67899eaa
Agam Bhardwaj <189280292+Overburn-06@users.noreply.github.com>
2026-05-13 09:21:38 +0530

    0404-SUM-OF-LEFT-LEAVES Stats: Time: 0 ms (100%), Space: 43.1 MB (66.22%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
@@ -13,44 +13,60 @@
  *     }
  * }
  */
-class Solution {
-    static int left_sum(TreeNode root,Queue<TreeNode>qu){
-        int sum=0;
-        qu.offer(root);
-        while(!qu.isEmpty()){
-            int size=qu.size();
-            ArrayList<Integer>temp=new ArrayList<>();
-            for(int i=0;i<size;i++){
-                TreeNode n=qu.poll();
+// class Solution {
+//     static int left_sum(TreeNode root,Queue<TreeNode>qu){
+//         int sum=0;
+//         qu.offer(root);
+//         while(!qu.isEmpty()){
+//             int size=qu.size();
+//             ArrayList<Integer>temp=new ArrayList<>();
+//             for(int i=0;i<size;i++){
+//                 TreeNode n=qu.poll();
                 
-                if(n.left!=null&&isLeaf(n.left)){
-                    temp.add(n.left.val);
-                }
-                if(n.left!=null){
-                    qu.offer(n.left);
-                }
-                if(n.right!=null){
-                    qu.offer(n.right);
-                }
-            }
-            for(int i=0;i<temp.size();i++){
-                sum+=temp.get(i);
-            }
-            // arr.add(temp);
-        }
-        // for(int i=0;i<arr.size();i++){
-        //     System.out.print(arr.get(i)+" ");
-        // }
-        return sum;
-    }
+//                 if(n.left!=null&&isLeaf(n.left)){
+//                     temp.add(n.left.val);
+//                 }
+//                 if(n.left!=null){
+//                     qu.offer(n.left);
+//                 }
+//                 if(n.right!=null){
+//                     qu.offer(n.right);
+//                 }
+//             }
+//             for(int i=0;i<temp.size();i++){
+//                 sum+=temp.get(i);
+//             }
+//             // arr.add(temp);
+//         }
+//         // for(int i=0;i<arr.size();i++){
+//         //     System.out.print(arr.get(i)+" ");
+//         // }
+//         return sum;
+//     }
+//     static boolean isLeaf(TreeNode root){
+//         if(root.left==null && root.right==null) return true;
+//         return false;
+//     }
+//     public int sumOfLeftLeaves(TreeNode root) {
+//         // ArrayList<ArrayList<Integer>>arr=new ArrayList<>();
+//         Queue<TreeNode>qu=new LinkedList<>();
+//         return left_sum(root,qu);
+    
+//     }
+// }
+
+class Solution {
+    
     static boolean isLeaf(TreeNode root){
         if(root.left==null && root.right==null) return true;
         return false;
     }
     public int sumOfLeftLeaves(TreeNode root) {
-        // ArrayList<ArrayList<Integer>>arr=new ArrayList<>();
-        Queue<TreeNode>qu=new LinkedList<>();
-        return left_sum(root,qu);
-    
+        if(root==null) return 0;
+        int sum=0;
+        if(root.left!=null&&isLeaf(root.left)){
+            sum+=root.left.val;
+        }
+        return sum+sumOfLeftLeaves(root.left)+sumOfLeftLeaves(root.right);
     }
 }
