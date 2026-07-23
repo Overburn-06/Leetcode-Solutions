8730cc171df425613e9965e8a2a0a2965418eb7f
Agam Bhardwaj <189280292+Overburn-06@users.noreply.github.com>
2026-07-23 07:43:30 +0530

    0462-MINIMUM-MOVES-TO-EQUAL-ARRAY-ELEMENTS-II Stats: Time: 11 ms (29.34%), Space: 47.1 MB (50.9%) - LeetHub

--- 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.java
+++ 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.java
@@ -1,27 +1,41 @@
+// class Solution {
+//     public int minMoves2(int[] nums) {
+//         Arrays.sort(nums);
+//         int r=nums[nums.length-1];
+//         int l=nums[0];
+//         int ans=0;
+//         while(l<=r){
+//             int mid=l+(r-l)/2;
+//             int total_mid=total_operation(nums,mid);
+//             int total_mid_one=total_operation(nums,mid+1);
+//             ans=Math.min(total_mid,total_mid_one);
+//             if(total_mid_one<total_mid){
+//                 l=mid+1;
+//             }else{
+//                 r=mid-1;
+//             }
+//         }
+//         return ans;
+//     }
+//     public static int total_operation(int []nums,int mid){
+//         int sum=0;
+//         for(int i=0;i<nums.length;i++){
+//             sum+=Math.abs(mid-nums[i]);
+//         }
+//         return sum;
+//     }
+// }
+
+
 class Solution {
     public int minMoves2(int[] nums) {
         Arrays.sort(nums);
-        int r=nums[nums.length-1];
-        int l=nums[0];
+        int med=nums[nums.length/2];
         int ans=0;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            int total_mid=total_operation(nums,mid);
-            int total_mid_one=total_operation(nums,mid+1);
-            ans=Math.min(total_mid,total_mid_one);
-            if(total_mid_one<total_mid){
-                l=mid+1;
-            }else{
-                r=mid-1;
-            }
+        for(int i=0;i<nums.length;i++){
+            ans+=Math.abs(med-nums[i]);
         }
         return ans;
     }
-    public static int total_operation(int []nums,int mid){
-        int sum=0;
-        for(int i=0;i<nums.length;i++){
-            sum+=Math.abs(mid-nums[i]);
-        }
-        return sum;
-    }
+   
 }
