466075bb99025b1454a56e1aac0136cd88af3bab
Agam Bhardwaj <189280292+Overburn-06@users.noreply.github.com>
2026-08-11 17:24:01 +0530

    1776-MINIMUM-OPERATIONS-TO-REDUCE-X-TO-ZERO Stats: Time: 4 ms (98.09%), Space: 102 MB (75.52%) - LeetHub

--- 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.java
+++ 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.java
@@ -1,7 +1,32 @@
+// class Solution {
+//     public int minOperations(int[] nums, int x) {
+//         HashMap<Integer,Integer>map=new HashMap<>();
+//         map.put(0,-1);
+//         int sum=0;
+//         for(int i:nums){
+//             sum+=i;
+//         }
+//         int target_sum=sum-x;
+//         if(target_sum==0) return nums.length;
+//         int count =Integer.MIN_VALUE;
+//         sum=0;
+//         for(int i=0;i<nums.length;i++){
+//             sum+=nums[i];
+//             int diff=sum-target_sum;
+//             if(map.containsKey(diff)){
+//                 count=Math.max(count,i-map.get(diff));
+//             }
+//             if(!map.containsKey(sum)){
+//                 map.put(sum,i);
+//             }
+//         }
+//         if(count==Integer.MIN_VALUE) return -1;
+//         return nums.length-count;
+//     }
+// }  
+
 class Solution {
     public int minOperations(int[] nums, int x) {
-        HashMap<Integer,Integer>map=new HashMap<>();
-        map.put(0,-1);
         int sum=0;
         for(int i:nums){
             sum+=i;
@@ -10,14 +35,16 @@ class Solution {
         if(target_sum==0) return nums.length;
         int count =Integer.MIN_VALUE;
         sum=0;
-        for(int i=0;i<nums.length;i++){
-            sum+=nums[i];
-            int diff=sum-target_sum;
-            if(map.containsKey(diff)){
-                count=Math.max(count,i-map.get(diff));
+        int left=0;
+        int right=0;
+        for(right=0;right<nums.length;right++){
+            sum+=nums[right];
+            while(left<=right && sum>target_sum){
+                sum-=nums[left];
+                left++;
             }
-            if(!map.containsKey(sum)){
-                map.put(sum,i);
+            if(sum==target_sum){
+                count=Math.max(count,right-left+1);
             }
         }
         if(count==Integer.MIN_VALUE) return -1;
