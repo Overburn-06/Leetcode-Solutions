e3bb496eac57b5cbf5c01ffc514c9e7929e229a5
Agam Bhardwaj <189280292+Overburn-06@users.noreply.github.com>
2026-08-13 15:30:20 +0530

    2552-MAXIMUM-SUM-OF-DISTINCT-SUBARRAYS-WITH-LENGTH-K Stats: Time: 75 ms (5.35%), Space: 102.5 MB (6.07%) - LeetHub

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -7,15 +7,15 @@ class Solution {
         long s=0;
         while(j<nums.length){
             map.put(nums[j],map.getOrDefault(nums[j],0)+1);
-            if(map.containsKey(nums[j]) && map.get(nums[j])==1){
+            if(map.get(nums[j])==1){
                 s+=(long)nums[j];
             }
             if((j-i+1)==k){
-                if(map.size()==k){
+                if(map.size()==k){ // this check that map contians only unique values
                     sum=Math.max(s,sum);
                 }
                 map.put(nums[i],map.get(nums[i])-1);
-                if(map.get(nums[i])==0){
+                if(map.get(nums[i])==0){ // if key frequency becomes zero than we should remove it otherwise size of map would not satisfy and it act as garbage value
                     map.remove(nums[i]);
                     s=s-nums[i];
                 }
