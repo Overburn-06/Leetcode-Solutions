8ffe59e577ee07a753fcffb8a431946aeaaac410
Agam Bhardwaj <189280292+Overburn-06@users.noreply.github.com>
2026-08-20 23:42:26 +0530

    2188-MINIMIZED-MAXIMUM-OF-PRODUCTS-DISTRIBUTED-TO-ANY-STORE Stats: Time: 45 ms (37.35%), Space: 80.8 MB (94.05%) - LeetHub

--- 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.java
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.java
@@ -2,14 +2,14 @@ class Solution {
     public int minimizedMaximum(int n, int[] quantities) {
         int left=1;
         int right=0;
-        for(int i=0;i<quantities.length;i++){
+        for(int i=0;i<quantities.length;i++){ //right can go upto maximum of quantities as maximum number of products of same type given to vendor could be max value
             right=Math.max(right,quantities[i]);
         }
         while(left<=right){
             int mid=left+(right-left)/2;
-            if(isPossible(quantities,n,mid)){
+            if(isPossible(quantities,n,mid)){// would check the count of vendor possible with mid and if less than or equal to n then we can check for more minimum value
                 right=mid-1;
-            }else{
+            }else{ 
                 left=mid+1;
             }
         }
@@ -18,11 +18,11 @@ class Solution {
     boolean isPossible(int []quantities,int n,int mid){
         int count=0;
         for(int i=0;i<quantities.length;i++){
-            count+=quantities[i]/mid;
+            count+=quantities[i]/mid;// count the number of possible vendors 
             if(quantities[i]%mid!=0){
                 count++;
             }
         }
-        return count<=n;
+        return count<=n; 
     }
 }
