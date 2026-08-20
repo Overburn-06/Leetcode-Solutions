3baf00f98bf7cf3a4b1bcf7e82d64ca2b20b6530
Agam Bhardwaj <189280292+Overburn-06@users.noreply.github.com>
2026-08-20 17:38:27 +0530

    2188-MINIMIZED-MAXIMUM-OF-PRODUCTS-DISTRIBUTED-TO-ANY-STORE Stats: Time: 46 ms (30.44%), Space: 81 MB (88.8%) - LeetHub

--- 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.java
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.java
@@ -5,17 +5,15 @@ class Solution {
         for(int i=0;i<quantities.length;i++){
             right=Math.max(right,quantities[i]);
         }
-        int res=0;
         while(left<=right){
             int mid=left+(right-left)/2;
             if(isPossible(quantities,n,mid)){
-                res=mid;
                 right=mid-1;
             }else{
                 left=mid+1;
             }
         }
-        return res;
+        return left;
     }
     boolean isPossible(int []quantities,int n,int mid){
         int count=0;
