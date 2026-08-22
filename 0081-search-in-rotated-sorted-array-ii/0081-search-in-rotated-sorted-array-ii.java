49a66c290654dd9f2a19c3d95f8d9203c37df0ce
Agam Bhardwaj <189280292+Overburn-06@users.noreply.github.com>
2026-08-22 18:39:13 +0530

    0081-SEARCH-IN-ROTATED-SORTED-ARRAY-II Stats: Time: 0 ms (100%), Space: 44.9 MB (66.42%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -5,20 +5,20 @@ class Solution {
         while(low<=high){
             int mid=low+(high-low)/2;
             if(nums[mid]==target) return true;
-            if(nums[mid]==nums[low] && nums[mid]==nums[high]){
+            if(nums[mid]==nums[low] && nums[mid]==nums[high]){// would check if low ,mid and high are of same then we cannt tell which one is sorted half so we would skip the duplicates by shrinking our array from both ends and would result in distinct elements
                 low++;
                 high--;
                 continue;
             }
-            if(nums[mid]>=nums[low]){
-                if(nums[low]<=target && nums[mid]>=target){
+            if(nums[mid]>=nums[low]){ // would check left side as it is sorted 
+                if(nums[low]<=target && nums[mid]>=target){// would divide left into half
                     high=mid-1;
                 }
                 else{
                     low=mid+1;
                 }
             }
-            else{
+            else{// if not left than right side should be checked 
                 if(nums[high]>=target && nums[mid]<=target){
                     low=mid+1;
                 }else{
