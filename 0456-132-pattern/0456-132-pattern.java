bae917a5eed5d61e6953abf17ce1b582272243ec
Agam Bhardwaj <189280292+Overburn-06@users.noreply.github.com>
2026-08-26 16:23:46 +0530

    0456-132-PATTERN Stats: Time: 40 ms (23.24%), Space: 98.6 MB (53.81%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.java
+++ 0456-132-pattern/0456-132-pattern.java
@@ -3,13 +3,13 @@ class Solution {
           Stack<Integer>st=new Stack<>();
           int num3=Integer.MIN_VALUE;
           for(int i=nums.length-1;i>=0;i--){
-            if(nums[i]<num3){
+            if(nums[i]<num3){ // if we already have jth and kth number so if any number less than kth number is found it satisfies our relation of nums[i]<nums[k]<nums[j] 
                 return true;
             }
-            while(!st.isEmpty() && st.peek()<nums[i]){
-                num3=st.pop();
+            while(!st.isEmpty() && st.peek()<nums[i]){ //would create a monotonic stack with increasing elements and if any number greater than top is found will pop and that number could be used for kth number and we had relation in which kth number < jth number
+                num3=Math.max(st.pop(),num3);
             }
-            st.push(nums[i]);
+            st.push(nums[i]); // else just push number into stack
           }
         return false;
     }
