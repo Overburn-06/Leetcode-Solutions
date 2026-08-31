48d4e246a0a821c9ccd7be78a15d0067d259e030
Agam Bhardwaj <189280292+Overburn-06@users.noreply.github.com>
2026-08-31 23:19:55 +0530

    2212-REMOVING-MINIMUM-AND-MAXIMUM-FROM-ARRAY Stats: Time: 2 ms (100%), Space: 86.6 MB (80.73%) - LeetHub

--- 2212-removing-minimum-and-maximum-from-array/2212-removing-minimum-and-maximum-from-array.java
+++ 2212-removing-minimum-and-maximum-from-array/2212-removing-minimum-and-maximum-from-array.java
@@ -16,13 +16,13 @@ class Solution {
             }
         }
         
-        int left=Math.min(min_i,max_i);
-        int right=Math.max(min_i,max_i);
+        int left=Math.min(min_i,max_i); //would have value of index on left 
+        int right=Math.max(min_i,max_i);//would have value of index on right side
 
-        int front=right+1;
-        int back=nums.length-left;
-        int both=(left+1)+(nums.length-right);
+        int front=right+1; // if both are on left side than index with greater vaue +1 would give total deletion
+        int back=nums.length-left; // if both are on the back than total length - the element on left max would give total deletion
+        int both=(left+1)+(nums.length-right); // if one is on left and other on right side then left +1 would give total deleteion on left and totol length - right index would give total deletion on right
         
-        return Math.min(both,Math.min(front,back));
+        return Math.min(both,Math.min(front,back)); // min of all gives minimum deletion
     }
 }
