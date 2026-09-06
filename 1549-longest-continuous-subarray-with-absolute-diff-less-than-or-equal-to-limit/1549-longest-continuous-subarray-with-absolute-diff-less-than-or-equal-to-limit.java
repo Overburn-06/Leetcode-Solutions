f7692fa37790f98f839ae16a615e9c023c2cdf91
Agam Bhardwaj <189280292+Overburn-06@users.noreply.github.com>
2026-09-06 12:48:25 +0530

    1549-LONGEST-CONTINUOUS-SUBARRAY-WITH-ABSOLUTE-DIFF-LESS-THAN-OR-EQUAL-TO-LIMIT Stats: Time: 114 ms (13.46%), Space: 101.4 MB (10.12%) - LeetHub

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -13,18 +13,18 @@ class Solution {
         PriorityQueue<Pair>max_heap=new PriorityQueue<>((a,b)->Integer.compare(b.value,a.value));
         PriorityQueue<Pair>min_heap=new PriorityQueue<>((a,b)->Integer.compare(a.value,b.value));
         for(int right=0;right<nums.length;right++){
-            max_heap.offer(new Pair(nums[right],right));
-            min_heap.offer(new Pair(nums[right],right));
-            while((!min_heap.isEmpty()&&!max_heap.isEmpty()) && Math.abs(max_heap.peek().value-min_heap.peek().value)>limit){
-                left=Math.min(min_heap.peek().index,max_heap.peek().index)+1;
-                while(!min_heap.isEmpty() && min_heap.peek().index<left){
+            max_heap.offer(new Pair(nums[right],right));//will maintain max element of current window at top
+            min_heap.offer(new Pair(nums[right],right));//will maintain min element of current window at top 
+            while((!min_heap.isEmpty()&&!max_heap.isEmpty()) && Math.abs(max_heap.peek().value-min_heap.peek().value)>limit){ // will move left until max-min<=limit
+                left=Math.min(min_heap.peek().index,max_heap.peek().index)+1;//will check for the min index to which we can jump as max can be first smaller index than min or min is at smaller index than max
+                while(!min_heap.isEmpty() && min_heap.peek().index<left){//will remove all the indexes which are now not in current window and will update min
                     min_heap.poll();
                 }
-                while(!max_heap.isEmpty() && max_heap.peek().index<left){
+                while(!max_heap.isEmpty() && max_heap.peek().index<left){//will remove all the indexes which are now not in current window and will update max
                     max_heap.poll();
                 }
             }
-            res=Math.max(res,right-left+1);
+            res=Math.max(res,right-left+1);//stores max length
         }
         return res;
     }
