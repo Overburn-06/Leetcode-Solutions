0c4ed931dcc45a68672d699281f3f68cc84e3c48
Agam Bhardwaj <189280292+Overburn-06@users.noreply.github.com>
2026-09-13 15:14:23 +0530

    0445-ADD-TWO-NUMBERS-II Stats: Time: 3 ms (42.35%), Space: 46.8 MB (14.5%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
@@ -10,66 +10,39 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        Stack<ListNode>st1=new Stack<>();
-        Stack<ListNode>st2=new Stack<>();
-        Stack<Integer>st=new Stack<>();
+        Stack<Integer>st1=new Stack<>();
+        Stack<Integer>st2=new Stack<>();
+
         ListNode t1=l1;
         ListNode t2=l2;
         while(t1!=null){
-            st1.push(t1);
+            st1.push(t1.val);
             t1=t1.next;
         }while(t2!=null){
-            st2.push(t2);
+            st2.push(t2.val);
             t2=t2.next;
         }
         int carry=0;
         ListNode res=new ListNode();
         ListNode curr=res;
-        while(!st1.isEmpty()&& !st2.isEmpty()){
-            int n1=st1.pop().val;
-            int n2=st2.pop().val;
-            int sum=n1+n2+carry;
-            if(sum>9){
-                carry=1;
-                sum=sum%10;
-            }else{
-                carry=0;
-            }
-            st.push(sum);
-        }
-        while(!st1.isEmpty()){
-            if(carry==1){
-                int s=st1.pop().val+1;
-                if(s>9){
-                    carry=1;
-                    st.push(s%10);
-                }else{
-                    st.push(s);
-                    carry=0;
-                }
-                continue;
-            }
-            st.push(st1.pop().val);
-        }
-        while(!st2.isEmpty()){
-            if(carry==1){
-                int s=st2.pop().val+1;
-                if(s>9){
-                    carry=1;
-                    st.push(s%10);
-                }else{
-                    st.push(s);
-                    carry=0;
-                }
-                continue;
-            }
-            st.push(st2.pop().val);
-        }
-        if(carry==1)st.push(1);
-        while(!st.isEmpty()){
-            curr.next=new ListNode(st.pop());
+        while(!st1.isEmpty() || !st2.isEmpty() || carry!=0){
+            int sum=carry;
+            if(!st1.isEmpty()) sum+=st1.pop();
+            if(!st2.isEmpty()) sum+=st2.pop();
+            int digit=sum%10;
+            carry=sum/10;
+            curr.next=new ListNode(digit);
             curr=curr.next;
         }
-        return res.next;
+         curr=res.next;
+        ListNode prev=null;
+        while(curr!=null){
+            ListNode next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
     }
+    
 }
