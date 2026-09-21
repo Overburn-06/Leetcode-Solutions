60e9f616fc9bf822c1d66a63b07e18f3b62fac50
Agam Bhardwaj <189280292+Overburn-06@users.noreply.github.com>
2026-09-21 17:16:04 +0530

    0776-N-ARY-TREE-POSTORDER-TRAVERSAL Stats: Time: 0 ms (100%), Space: 46.3 MB (88.82%) - LeetHub

--- 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.java
+++ 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.java
@@ -17,28 +17,43 @@ class Node {
 }
 */
 
+// class Solution {
+//     public List<Integer> postorder(Node root) {
+//         List<Integer>res=new ArrayList<>();
+//         if(root==null)return res;
+//         n_post(root,res);
+//         return res;
+//     }
+//     public void n_post(Node root,List<Integer>res){
+//         Stack<Node>st1=new Stack<>();
+//         st1.push(root);
+//         while(!st1.isEmpty()){
+//             Node temp=st1.pop();
+//             for(Node node:temp.children){
+//                 if(node!=null){
+//                     st1.push(node);
+//                 }
+//             }
+//             res.add(temp.val);
+//         }
+//         Collections.reverse(res);
+//     }
+// }
+
+
 class Solution {
     public List<Integer> postorder(Node root) {
         List<Integer>res=new ArrayList<>();
         if(root==null)return res;
         n_post(root,res);
+        res.add(root.val);
         return res;
     }
     public void n_post(Node root,List<Integer>res){
-        Stack<Node>st1=new Stack<>();
-        Stack<Node>st2=new Stack<>();
-        st1.push(root);
-        while(!st1.isEmpty()){
-            Node temp=st1.pop();
-            for(Node node:temp.children){
-                if(node!=null){
-                    st1.push(node);
-                }
-            }
-            st2.push(temp);
-        }
-        while(!st2.isEmpty()){
-            res.add(st2.pop().val);
+        if(root==null) return;
+        for(Node node:root.children){
+            n_post(node,res);
+            res.add(node.val);
         }
     }
 }
