5220063b64e28bf85a38609213cea62d33708646
Agam Bhardwaj <189280292+Overburn-06@users.noreply.github.com>
2026-09-21 16:47:19 +0530

    0226-INVERT-BINARY-TREE Stats: Time: 0 ms (100%), Space: 42.9 MB (65.25%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.java
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -13,29 +13,45 @@
  *     }
  * }
  */
+// class Solution {
+//     public TreeNode invertTree(TreeNode root) {
+//         if(root==null) return root;
+//        Queue<TreeNode>qu=new LinkedList<>();
+//        qu.offer(root);
+//        while(!qu.isEmpty()){
+//         int n=qu.size();
+//         while(n>0){
+//             TreeNode temp=qu.poll();
+//             if(temp.left!=null || temp.right!=null){
+//                 TreeNode t=temp.left;
+//                 temp.left=temp.right;
+//                 temp.right=t;
+//             }
+//             if(temp.left!=null){
+//                 qu.offer(temp.left);
+//             }
+//             if(temp.right!=null){
+//                 qu.offer(temp.right);
+//             }
+//             n--;
+//         }
+//        }
+//        return root;
+//     }
+// }
+
+
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        if(root==null) return root;
-       Queue<TreeNode>qu=new LinkedList<>();
-       qu.offer(root);
-       while(!qu.isEmpty()){
-        int n=qu.size();
-        while(n>0){
-            TreeNode temp=qu.poll();
-            if(temp.left!=null || temp.right!=null){
-                TreeNode t=temp.left;
-                temp.left=temp.right;
-                temp.right=t;
-            }
-            if(temp.left!=null){
-                qu.offer(temp.left);
-            }
-            if(temp.right!=null){
-                qu.offer(temp.right);
-            }
-            n--;
+        if(root==null){
+            return root;
         }
-       }
-       return root;
+        TreeNode temp=root.left;
+        root.left=root.right;
+        root.right=temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
     }
 }
