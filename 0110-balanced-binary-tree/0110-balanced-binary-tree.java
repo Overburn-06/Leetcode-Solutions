c3ed21ba3cff944bb8cb9baa18ad8924e49f1722
Agam Bhardwaj <189280292+Overburn-06@users.noreply.github.com>
2026-09-22 14:56:04 +0530

    0110-BALANCED-BINARY-TREE Stats: Time: 0 ms (100%), Space: 45.4 MB (77.07%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -14,17 +14,17 @@
  * }
  */
 class Solution {
-    static int height(TreeNode root){
-        if(root==null) return 0;
-
+    public int height(TreeNode root){
+        if(root==null)return 0;
         int lh=height(root.left);
-        if(lh==-1) return -1;
         int rh=height(root.right);
-        if(rh==-1) return -1;
-        if(Math.abs(lh-rh)>1)return -1;
         return Math.max(lh,rh)+1;
     }
     public boolean isBalanced(TreeNode root) {
-        return height(root)!=-1;
+        if(root==null)return true;
+        int lh=height(root.left);
+        int rh=height(root.right);
+        if(Math.abs(lh-rh)>1)return false;
+        return isBalanced(root.left) && isBalanced(root.right);
     }
 }
