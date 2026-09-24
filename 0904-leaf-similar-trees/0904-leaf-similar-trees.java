c0c7c878cefc835820b99133e2a7df38406daebd
Agam Bhardwaj <189280292+Overburn-06@users.noreply.github.com>
2026-09-25 02:27:24 +0530

    0904-LEAF-SIMILAR-TREES Stats: Time: 0 ms (100%), Space: 43.1 MB (84.38%) - LeetHub

--- 0904-leaf-similar-trees/0904-leaf-similar-trees.java
+++ 0904-leaf-similar-trees/0904-leaf-similar-trees.java
@@ -14,33 +14,11 @@
  * }
  */
 class Solution {
-    // public List<Integer> bfs(TreeNode root){
-    //     List<Integer>arr=new ArrayList<>();
-    //     Queue<TreeNode>qu=new LinkedList<>();
-    //     qu.offer(root);
-    //     while(!qu.isEmpty()){
-    //         int n=qu.size();
-    //         while(n>0){
-    //             TreeNode temp=qu.poll();
-    //             if((temp.left==null && temp.right==null)){
-    //                 arr.add(temp.val);
-    //             }
-    //             if(temp.left!=null){
-    //                 qu.offer(temp.left);
-    //             }
-    //             if(temp.right!=null){
-    //                 qu.offer(temp.right);
-    //             }
-    //             n--;
-    //         }
-    //     }
-    //     return arr;
-    // }
     public void dfs(TreeNode root,List<Integer>arr){
         if(root==null) return;
         dfs(root.left,arr);
-        dfs(root.right,arr);
         if(root.left==null && root.right==null) arr.add(root.val);
+        dfs(root.right,arr);
     }
         public boolean leafSimilar(TreeNode root1, TreeNode root2) {
             List<Integer>r1=new ArrayList<>();
